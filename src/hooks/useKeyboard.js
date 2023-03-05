c64d1b4cbdd9288e7cd23f2318afd1c43fa036fb
Keyan5 <keyan5694@gmail.com>
2023-03-05 23:35:54 +0530

    First Commit

---
+++ src/hooks/useKeyboard.js
@@ -0,0 +1,55 @@
+import { useCallback, useEffect, useState } from "react";
+
+const actionByKey = (key)=>{
+    const actionKeyMap = {
+        KeyW: 'moveForward',
+        KeyS: 'moveBackward',
+        KeyD: 'moveRight',
+        KeyA: 'moveLeft',
+        Space: 'jump',
+        Digit1: 'dirt',
+        Digit2: 'grass',
+        Digit3: 'glass',
+        Digit4: 'wood',
+        Digit5: 'log'
+    }
+    return actionKeyMap[key];
+}
+
+export const useKeyboard = ()=>{
+    const [actions, setActions] = useState({
+        moveForward: false,
+        moveBackward: false,
+        moveRight: false,
+        moveLeft: false,
+        jump: false,
+        dirt: false,
+        grass: false,
+        glass: false,
+        wood: false,
+        log: false,
+    });
+
+    const handleKeyDown = useCallback((e)=>{
+        const action = actionByKey(e.code);
+        if(action)
+            setActions((prev)=>({...prev,[action]:true}));
+    },[]);
+
+    const handleKeyUp = useCallback((e)=>{
+        const action = actionByKey(e.code);
+        if(action)
+            setActions((prev)=>({...prev,[action]:false}));
+    },[]);
+
+    useEffect(()=>{
+        document.addEventListener('keydown',handleKeyDown);
+        document.addEventListener('keyup',handleKeyUp);
+        return  ()=>{
+            document.removeEventListener('keydown',handleKeyDown);
+            document.removeEventListener('keyup',handleKeyUp);
+        }
+    },[handleKeyDown,handleKeyUp]);
+    
+    return actions;
+}
